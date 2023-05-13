19a74ec2db8041129197f238cd4e972df53784b4
endaaman <ken@endaaman.me>
2023-05-14 00:29:38 +0900

    update

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from PIL import Image
 from PIL.Image import Image as ImageType
 
 from endaaman.ml import pil_to_tensor
+from endaaman import grid_split
 
 
 def overlay_heatmap(mask: torch.Tensor, img: torch.Tensor, alpha=1.0, threshold=0.2, cmap=cv2.COLORMAP_JET):
@@ -52,59 +53,6 @@ def concat_grid_images_float(images, n_cols=1):
     return bg
 
 
-
-def select_side(W, w, idx=None):
-    count = int(np.ceil(W / w))
-    if count <= 1:
-        return 0
-    overwrap = (w * count - W) / (count - 1)
-    if idx is None:
-        idx = np.random.randint(0, count)
-    return int(idx * w - overwrap * idx)
-
-def grid_split_with_overwrap(img, size, flattern=False):
-    hor_count = int(np.ceil(img.width / size))
-    ver_count = int(np.ceil(img.height / size))
-    iii = []
-    for v_idx in range(ver_count):
-        ii = []
-        for h_idx in range(hor_count):
-            x = select_side(img.width, size, h_idx)
-            y = select_side(img.height, size, v_idx)
-            ii.append(img.crop((x, y, x+size, y+size)))
-        iii.append(ii)
-
-    if flattern:
-        iii = list(itertools.chain.from_iterable(iii))
-    return iii
-
-def n_split(x, n):
-    return [(x + i) // n for i in range(n)]
-
-def grid_split_by_size(img, size, flattern=False):
-    hor_sizes = n_split(img.width, max(img.width//size, 1))
-    ver_sizes = n_split(img.height, max(img.height//size, 1))
-    iii = []
-    y = 0
-
-    for ver_size in ver_sizes:
-        ii = []
-        x = 0
-        for hor_size in hor_sizes:
-            ii.append(img.crop((x, y, x+hor_size, y+ver_size)))
-            x += hor_size
-        y += ver_size
-        iii.append(ii)
-
-    if flattern:
-        iii = list(itertools.chain.from_iterable(iii))
-    return iii
-
-
-def grid_split(img, size, overwrap=True, flattern=False):
-    if overwrap:
-        return grid_split_with_overwrap(img, size, flattern)
-    return grid_split_by_size(img, size, flattern)
 
 
 def test_grid():
