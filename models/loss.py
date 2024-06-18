46480ebe13b9944766857ba207f2322f0447fad9
endaaman <ken@endaaman.com>
2024-06-18 10:59:46 +0900

    fix

--- models/loss.py
+++ models/loss.py
@@ -119,7 +119,7 @@ class BarlowTwinsLoss(nn.Module):
         super().__init__()
         self.lambd = lambd
 
-    def off_diagonal(x):
+    def off_diagonal(self, x):
         n, m = x.shape
         assert n == m
         return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
