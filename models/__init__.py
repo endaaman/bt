49b1e85e3f73f6b51cbad7ad1baae1290916b6a1
endaaman <ken@endaaman.com>
2024-08-06 19:35:40 +0900

    ctranspath

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ from typing import NamedTuple, Callable
 
 import torch
 import timm
-from timm.models.layers import to_2tuple
+from timm.models.layers import to_2tuple, PatchEmbed
 from torch import nn
 from torch.nn import functional as F
 import torchvision as tv
@@ -14,6 +14,7 @@ from pydantic import Field
 
 from endaaman.ml import BaseMLCLI
 
+from .ctranspath import ctranspath
 from .loss import NestedCrossEntropyLoss, CrossEntropyLoss, SymmetricCosSimLoss, BarlowTwinsLoss
 
 
@@ -68,42 +69,6 @@ class TimmModel(nn.Module):
             return x, features
         return x
 
-class ConvStem(nn.Module):
-    def __init__(self, img_size=224, patch_size=4, in_chans=3, embed_dim=768, norm_layer=None, flatten=True, output_fmt=None):
-        super().__init__()
-        assert patch_size == 4
-        assert embed_dim % 8 == 0
-        img_size = to_2tuple(img_size)
-        patch_size = to_2tuple(patch_size)
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
-        self.num_patches = self.grid_size[0] * self.grid_size[1]
-        self.flatten = flatten
-        stem = []
-        input_dim, output_dim = 3, embed_dim // 8
-        for l in range(2):
-            stem.append(nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=2, padding=1, bias=False))
-            stem.append(nn.BatchNorm2d(output_dim))
-            stem.append(nn.ReLU(inplace=True))
-            input_dim = output_dim
-            output_dim *= 2
-        stem.append(nn.Conv2d(input_dim, embed_dim, kernel_size=1))
-        self.proj = nn.Sequential(*stem)
-
-        self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
-
-    # def forward(self, x):
-    #     B, C, H, W = x.shape
-    #     assert H == self.img_size[0] and W == self.img_size[1], \
-    #         f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
-    #     x = self.proj(x)
-    #     if self.flatten:
-    #         x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
-    #     x = self.norm(x)
-    #     return x
-
-
 class CompareModel(nn.Module):
     def __init__(self, num_classes, frozen=False, base='random'):
         super().__init__()
@@ -132,16 +97,12 @@ class CompareModel(nn.Module):
             # 'vit_large_patch16_224'
             self.base = timm.create_model('hf-hub:MahmoodLab/uni', pretrained=True, init_values=1e-5)
         elif base == 'ctranspath':
-            # self.base = timm.create_model('swin_tiny_patch4_window7_224', embed_layer=ConvStem, pretrained=False)
-            self.base = timm.create_model('swin_tiny_patch4_window7_224', pretrained=False, embed_dim=48)
-            self.base.head = nn.Identity()
-            td = torch.load('./data/weights/ctranspath.pth')
-            self.base.load_state_dict(td['model'], strict=False)
-            self.pool = nn.Sequential(
-                tv.ops.Permute([0, 3, 1, 2]),
-                nn.AdaptiveAvgPool2d((1, 1)),
-                nn.Flatten(start_dim=1),
-            )
+            self.base = ctranspath()
+            # self.pool = nn.Sequential(
+            #     tv.ops.Permute([0, 3, 1, 2]),
+            #     nn.AdaptiveAvgPool2d((1, 1)),
+            #     nn.Flatten(start_dim=1),
+            # )
         else:
             raise RuntimeError('Invalid base:', base)
 
